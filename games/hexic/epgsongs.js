771e96b3c0a280bb083775d5f04148d2cb684491
barney-heatwave <djdise@outlook.com>
2020-06-30 15:59:00 +0100

    Test Hexic HD music

---
+++ games/hexic/epgsongs.js
@@ -0,0 +1,16 @@
+var lastSong = null;
+var selection = null;
+var playlist = ["Oilyn.wma", "Underlow.wma", "Slartisfgh.wma", "Cobblers.wma", "Belkell.wma", "Lubax.wma", "Badback.wma", "54.wma",]; // List of Songs
+var player = document.getElementById("audioplayer"); // Get Audio Element
+player.autoplay = true;
+player.addEventListener("ended", selectRandom); // Run function when song ends
+
+function selectRandom() {
+    while (selection == lastSong) { // Repeat until different song is selected
+        selection = Math.floor(Math.random() * playlist.length);
+    }
+    lastSong = selection; // Remember last song
+    player.src = playlist[selection]; // Tell HTML the location of the new Song
+}
+
+selectRandom(); // Select initial song
